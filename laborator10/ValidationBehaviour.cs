47c2d2156751b06c4335345c91575cd66e564643
FelixCrisan <crisanfelix@yahoo.com>
2021-12-10 18:08:13 +0200

    felixcrisanLab11

---
+++ laborator10/ValidationBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using laborator10.Data;
+using laborator10.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace laborator10
+{
+    class ValidationBehaviour : Behavior<Editor>
+    {
+        protected override void OnAttachedTo(Editor entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+        protected override void OnDetachingFrom(Editor entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            ((Editor)sender).BackgroundColor =
+            string.IsNullOrEmpty(args.NewTextValue) ? Color.Red : Color.Default;
+        }
+    }
+}
